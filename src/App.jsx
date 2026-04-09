b5e57bfa1c52145361136c317e3f60e7945da017
Pradeep Mali <malipradeep591@gmail.com>
2026-04-09 16:17:29 +0530

    Style: fix main dashboard layout bugs and establish devpulse theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,22 @@ function App(){
   return (
     <div className="min-h-screen bg-devpulse-dark text-slate-200 font-sans">
 
-      <nav className='border-b border-slate-800 bg-devpulse-card px-6 py-4 flex justify-between item-center shadow-Lg'>
-        <h1 className='text-2x1 font-bold bg-gradient-to-r from-devpulse-glow to-blue-400 bg-clip-text text-transparent'>DevPulse</h1>
+      <nav className='border-b border-slate-800 bg-devpulse-card px-6 py-4 flex justify-between items-center shadow-Lg'>
+        <h1 className='text-2xl font-bold bg-gradient-to-r from-devpulse-glow to-blue-400 bg-clip-text text-transparent'>DevPulse</h1>
         <div className='text-sm font-medium text-slate-400'>Github Visualizer</div>
       </nav>
 
-      <main className='max-w-7x1 mx-auto p-6 mt-8'>
+      <main className='max-w-7xl mx-auto p-6 mt-8'>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          <div className='big-devpulse-card p-6 rounded-x1 border border-slate-800 shadow-x1 h-64 flex items-center justify-center'>
-            <span className='text-scale-500 font-medium'>Profile Panel</span>
 
-            <div className='big-devpulse-card p-6 rounded-x1 border border-slate-800 shadow-x1 h-64 flex items-center justify-center col-span-1 md:col-span-2'>
-              <span className='text-slate-500 font-medium'>Galaxy Canvas</span>
-            </div>
-
+          <div className='bg-devpulse-card p-6 rounded-xl border border-slate-800 shadow-xl h-64 flex items-center justify-center'>
+            <span className='text-slate-500 font-medium'>Profile Panel</span>
           </div>
+
+          <div className='bg-devpulse-card p-6 rounded-xl border border-slate-800 shadow-xl h-64 flex items-center justify-center col-span-1 md:col-span-2'>
+            <span className='text-slate-500 font-medium'>Galaxy Canvas</span>
+          </div>
+
         </div>
       </main>
     </div>
