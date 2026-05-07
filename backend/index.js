13069934fbd3df0cbf076c402ec12a8225e16cd0
Pradeep Mali <malipradeep591@gmail.com>
2026-05-07 21:46:45 +0530

    move github api to backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+const githubRoutes = require("./routes/githubRoutes");
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const Developer = require('./models/Developer');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +12,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/github", githubRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
@@ -69,6 +73,7 @@ app.post('/api/leaderboard', async (req, res) => {
 app.get("/", (req, res) => {
     res.send("DevPulse Backend Running")
 })
+
 app.listen(PORT, () => {
   console.log(`🚀 DevPulse Backend running on http://localhost:${PORT}`);
 });
